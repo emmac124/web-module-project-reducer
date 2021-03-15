1aef15f9a2380c9de1967c0e0c7bd6520c53552e
emmac124 <emma.cooperhawkins@gmail.com>
2021-03-15 13:10:36 -0700

    completed mvp

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,10 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY = "MEMORY";
+export const MR = "MR";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +13,23 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operation) => {
+    return({type: CHANGE_OPERATION, payload: operation});
+}
+
+export const clear = () => {
+    return({type: CLEAR_DISPLAY})
+}
+
+export const memory = () => {
+    return({type: MEMORY})
+}
+
+export const mR = () => {
+    return({type: MR})
+}
+export const memoryClear = () => {
+    return({type: MEMORY_CLEAR})
 }
